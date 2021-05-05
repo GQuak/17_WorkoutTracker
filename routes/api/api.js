e1dcff1b2070171279ef1d99579827b89bb6b5e3
GQuak <75180482+GQuak@users.noreply.github.com>
2021-05-04 21:37:58 -0600

    finalizing route updates

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,21 +2,15 @@
 const router = require("express").Router();
 const Workout = require("../../models/workout.js");
 
-// Post new workout
-router.post("/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-// Get all workouts, and sort by most recent
-router.get("/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ day: "asc" })
+// Get workout dashboard using aggregate, addFields, and sum
+router.get("/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -44,15 +38,21 @@ router.put("/workouts/:id", (req, res) => {
     });
 });
 
-// Get workout dashboard using aggregate, addFields, and sum
-router.get("/workouts/range", (req, res) => {
-  Workout.aggregate([
-    {
-      $addFields: {
-        totalDuration: { $sum: "$exercises.duration" },
-      },
-    },
-  ])
+// Get all workouts, and sort by most recent
+router.get("/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ day: "asc" })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// Post new workout
+router.post("/workouts", ({ body }, res) => {
+  Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
